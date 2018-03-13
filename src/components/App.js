fb52aadb4c2502b4d889a8409a318e5e1d7927df
Anmol Desai <anmoldesai4@gmail.com>
2018-03-13 15:13:59 +1300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import '../styles/index.css';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Lendi!</h2>
+      </div>
+    )
+  }
+}
+
+
+export default App;
